b8f17c4d326f399ccd87512a257f45b6abe2190c
david0borka <david0borka@gmail.com>
2021-11-21 20:44:08 +0100

    Adding Movies.API

---
+++ src/SecureMicroservices/Movies.API/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using Movies.API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MoviesContext>(options =>
+{
+	options.UseInMemoryDatabase("Movies");
+});
+var app = builder.Build();
+MovieContextSeed.SeedDatabase(app);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
